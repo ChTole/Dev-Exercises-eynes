dd879db854edf8d285afaa863724995d764e08b6
ChTole <ch.ariel.tole@gmail.com>
2022-12-06 17:39:14 -0300

    Búsqueda de secuencia vertical

--- Matriz/matriz.py
+++ Matriz/matriz.py
@@ -12,5 +12,11 @@ for k in matriz:
 for i in range(len(matriz)):
     for j in range(len(matriz[i]) - 3):
         if matriz[i][j] == matriz[i][j + 1] - 1 == matriz[i][j + 2] - 2 == matriz[i][j + 3] - 3:
-            inicia = f"fila {i + 1} - columna {j + 1}"
-            final = f"fila {i + 1} - columna {j+3 + 1}"
+            inicia_h = f"fila {i + 1} - columna {j + 1}"
+            final_h = f"fila {i + 1} - columna {j+3 + 1}"
+            
+for i in range(len(matriz) - 3):
+    for j in range(len(matriz[i])):
+        if matriz[i][j] == matriz[i + 1][j] - 1 == matriz[i + 2][j] - 2 == matriz[i + 3][j] - 3:
+            inicia_v = f"fila {i + 1} - columna {j + 1}"
+            final_v = f"fila {i+3 + 1} - columna {j + 1}"
