808b30b2140e3949c68c5dea7c56117532283361
ChTole <ch.ariel.tole@gmail.com>
2022-12-06 17:53:04 -0300

    Impido instancias de Circulo con radio <= 0

--- Clases/clases.py
+++ Clases/clases.py
@@ -2,8 +2,21 @@ from math import pi
 
 class Circulo:
     
+    def comprobar_cero(numero):
+        if numero <= 0:
+            raise ValueError
+        else:
+            return numero
+        
     def __init__(self, radio):
-        self.radio = radio
+        try:
+            self.radio = Circulo.comprobar_cero(radio)
+        except ValueError as e:
+            print(f"""
+            {type(e).__name__} 
+            Radio: {radio}
+            No se puede crear un círculo con radio igual o menor a 0.
+            """)
         
     def area(self, ):
         return round(pi * self.radio ** 2, 2)
