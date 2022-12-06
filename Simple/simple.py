c55d627f55fbd8e9cd93d60ab8c589970fcd1ee0
ChTole <ch.ariel.tole@gmail.com>
2022-12-06 17:34:45 -0300

    Ordeno por edad y muestro id de menor y de mayor

--- Simple/simple.py
+++ Simple/simple.py
@@ -5,3 +5,13 @@ def generar_edades():
     for i in range(1, 11):
         lista.append(dict(id = i, edad = random.randint(1,100)))  
     return lista
+
+def ordenar_edades(lista):
+    def capturar_edad(edad):
+        return edad['edad']
+    lista.sort(reverse=True, key=capturar_edad)
+    print("id persona más joven: ", lista[-1]["id"])
+    print("id persona más anciana: ", lista[0]["id"])
+    return lista
+
+edades = ordenar_edades(generar_edades())
