2410043d6f6080fc4c0768bd7f782b321b7e0c73
ChTole <ch.ariel.tole@gmail.com>
2022-12-06 17:38:23 -0300

    Búsqueda de secuencia horizontal

--- Matriz/matriz.py
+++ Matriz/matriz.py
@@ -8,3 +8,9 @@ for i in range(5):
 
 for k in matriz:
     print(k)
+    
+for i in range(len(matriz)):
+    for j in range(len(matriz[i]) - 3):
+        if matriz[i][j] == matriz[i][j + 1] - 1 == matriz[i][j + 2] - 2 == matriz[i][j + 3] - 3:
+            inicia = f"fila {i + 1} - columna {j + 1}"
+            final = f"fila {i + 1} - columna {j+3 + 1}"
